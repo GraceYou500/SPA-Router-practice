e3d8c66760dfbc8a8879c05bdb9ef3a5efce2322
Grace You <graceyouya500@gmail.com>
2022-08-25 18:45:52 +1000

    add details

--- 09-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ 09-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Grace', text: 'Learning JS is Great!' },
+  { id: 'q3', author: 'Mia', text: 'Learning Eating is fun!' },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- 09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ 09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,11 +1,25 @@
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Grace', text: 'Learning JS is Great!' },
+  { id: 'q3', author: 'Mia', text: 'Learning Eating is fun!' },
+];
 
 const QuoteDetail = () => {
   const Params = useParams();
+  const quote = DUMMY_QUOTES.find(quote => quote.id === Params.quoteId);
+
+  if (!quote) {
+    return <NoQuotesFound />;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
+      <HighlightedQuote author={quote.author} text={quote.text} />
       <p>{Params.quoteId}</p>
       <Route path={`/quotes/${Params.quoteId}/comments`}>
         <Comments />
