60895c3d199d480b0359672931889e16136d2454
Grace You <graceyouya500@gmail.com>
2022-08-28 20:54:26 +1000

    adding comments

--- 09-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
+++ 09-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
@@ -1,12 +1,31 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 import classes from './NewCommentForm.module.css';
 
-const NewCommentForm = (props) => {
+const NewCommentForm = props => {
   const commentTextRef = useRef();
+  const Params = useParams();
+  console.log(Params);
+  const { sendRequest, status, error } = useHttp(addComment);
 
-  const submitFormHandler = (event) => {
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
+
+  const submitFormHandler = event => {
     event.preventDefault();
+
+    const enteredText = commentTextRef.current.value;
+
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
 
     // optional: Could validate here
 
@@ -15,12 +34,17 @@ const NewCommentForm = (props) => {
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
